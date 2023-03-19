dbe7dd84468d43520a8e33a3421c2201994283ae
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-03-19 23:31:19 +0800

    Added new contract

--- 10 Re-entrancy/Re-entrancy.sol
+++ 10 Re-entrancy/Re-entrancy.sol
@@ -1,30 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import 'openzeppelin-contracts-06/math/SafeMath.sol';
+import "openzeppelin-contracts-06/math/SafeMath.sol";
 
 contract Reentrance {
-  
-  using SafeMath for uint256;
-  mapping(address => uint) public balances;
+    using SafeMath for uint256;
+    mapping(address => uint) public balances;
 
-  function donate(address _to) public payable {
-    balances[_to] = balances[_to].add(msg.value);
-  }
-
-  function balanceOf(address _who) public view returns (uint balance) {
-    return balances[_who];
-  }
-
-  function withdraw(uint _amount) public {
-    if(balances[msg.sender] >= _amount) {
-      (bool result,) = msg.sender.call{value:_amount}("");
-      if(result) {
-        _amount;
-      }
-      balances[msg.sender] -= _amount;
+    function donate(address _to) public payable {
+        balances[_to] = balances[_to].add(msg.value);
     }
-  }
 
-  receive() external payable {}
+    function balanceOf(address _who) public view returns (uint balance) {
+        return balances[_who];
+    }
+
+    function withdraw(uint _amount) public {
+        if (balances[msg.sender] >= _amount) {
+            (bool result, ) = msg.sender.call{value: _amount}("");
+            if (result) {
+                _amount;
+            }
+            balances[msg.sender] -= _amount;
+        }
+    }
+
+    receive() external payable {}
+}
+
+contract Hack {
+    Reentrance reentrance;
+
+    constructor(address _target) {
+        Reentrance = reentrance(_target);
+    }
+
+    function attack() public payable {}
 }
